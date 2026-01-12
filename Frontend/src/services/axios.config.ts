d51df7d15b7e8a250a7ca124ddd7996884d56685
Sparsh Gupta <sparshgupta78970@gmail.com>
2026-01-12 18:29:24 +0530

    Configure Frontend to use environment-based backend URL

--- Frontend/src/services/axios.config.ts
+++ Frontend/src/services/axios.config.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { TokenType } from '../types/token.type'
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
